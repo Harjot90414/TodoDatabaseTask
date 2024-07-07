03e48bdf27a3718aa6119f57379c560e6dc52b55
harjots90414 <harjot90414singh@gmail.com>
2024-07-07 09:52:07 +0530

    Delete Item Crash Solved

--- app/src/main/java/com/harjot/tododatabasetask/Notes/NotesEntity.kt
+++ app/src/main/java/com/harjot/tododatabasetask/Notes/NotesEntity.kt
@@ -2,16 +2,17 @@ package com.harjot.tododatabasetask.Notes
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.harjot.tododatabasetask.TodoEntity
 
-@Entity(foreignKeys = arrayOf(
-    ForeignKey(
-        TodoEntity::class,
-        parentColumns = ["id"],
-        childColumns = ["notesId"]
-        )
-    )
+@Entity(foreignKeys = [ForeignKey(
+    TodoEntity::class,
+    parentColumns = ["id"],
+    childColumns = ["notesId"],
+    onDelete = ForeignKey.CASCADE
+    )],
+    indices = [Index(value = ["notesId"])]
 )
 data class NotesEntity(
     @PrimaryKey(autoGenerate = true)
